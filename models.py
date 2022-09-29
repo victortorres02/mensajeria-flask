f38ecb6461c82b3423eede2039348fb9c9116dda
Victor Torres <Zorro4@LIVE.COM.MX>
2022-09-29 07:02:45 -0700

    Añadido modelo de mensajes

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, TIMESTAMP, LargeBinary
 from sqlalchemy.orm import relationship
 from database import Base
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -10,6 +11,8 @@ class User(Base):
     hashed_password = Column(String(127), nullable=False)
     sessions = relationship(
             'SessionId', back_populates='user', cascade='all, delete-orphan')
+    chats = relationship(
+            'UserChat', secondary='user_chat_membership', back_populates='members')
     def __repr__(self):
         return f'<User {self.username} ({self.email})>'
 
@@ -22,6 +25,32 @@ class SessionId(Base):
     expire_time   = Column(TIMESTAMP, nullable=False)
     user = relationship (
             'User', back_populates='sessions')
+
+class UserChat(Base):
+    __tablename__ = 'user_chats'
+    id = Column(Integer, primary_key=True)
+    members = relationship(
+            'User', secondary='user_chat_membership', back_populates='chats')
+    messages = relationship(
+            'ChatMessage', back_populates='chat', cascade='all, delete-orphan')
+
+user_chat_membership = Table(
+        'user_chat_membership',
+        Base.metadata,
+        Column('user_id', ForeignKey('users.id'), nullable=False, primary_key=True),
+        Column('chat_id', ForeignKey('user_chats.id'), nullable=False, primary_key=True)
+        )
+
+class ChatMessage(Base):
+    __tablename__ = 'chat_messages'
+    id = Column(Integer, nullable=False, primary_key=True)
+    chat_id = Column(Integer, ForeignKey('user_chats.id'), nullable=False, primary_key=True)
+    sender_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    message_type = Column(Integer, nullable=False)
+    message_data = Column(LargeBinary, nullable=False)
+    send_time = Column(TIMESTAMP, nullable=False)
+    chat = relationship (
+            'UserChat', back_populates='messages')
 
 class InvalidCredentialsError(Exception):
     pass
