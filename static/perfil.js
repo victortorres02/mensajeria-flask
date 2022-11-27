9d0c0af0ee58b9332d3aa896538327c8ea45c3fe
Fierran <ferransuabc@gmail.com>
2022-11-27 14:48:01 -0800

    agregue el href de salir y perfil

---
+++ static/perfil.js
@@ -0,0 +1,7 @@
+import { get_self_user_data } from "./webclient/profile";
+
+function setup() {
+    username.textContent = 'comes';
+}
+
+window.onload = setup;
--- static/webclient/profile.js
+++ static/webclient/profile.js
@@ -1,7 +1,10 @@
 import api_call from "./api_call.js";
 
-export async function get_self_user_id() {
-	return api_call("/api/web/get_profile_data", new Object())
-	.then(json_res => json_res.user_id);
+export async function get_self_user_data() {
+	return api_call("/api/web/get_profile_data", new Object());
 }
 
+export async function get_self_user_id() {
+	return get_self_user_data()
+	.then(profile_data => profile_data.user_id);
+}
